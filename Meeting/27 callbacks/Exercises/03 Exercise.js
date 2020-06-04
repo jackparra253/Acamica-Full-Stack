5510deb88c490479513110c67e297163d4fc0941
Jack <jack.parra253@gmail.com>
2020-06-04 12:26:25 -0500

    calculate prime numbers

---
+++ Meeting/27 callbacks/Exercises/03 Exercise.js
@@ -0,0 +1,21 @@
+const callback = function (number) {
+    let count = 0;
+
+    for (let i = 1; i <= number; i++) {
+        if(number%i  == 0) 
+            count++;
+    }
+
+    if (count === 2)
+        console.log(`El ${number} es primo`);
+    else {
+        console.log(`El ${number} no es primo.`);
+    }
+}
+
+
+const calc = (number, callback) => callback(number);
+
+calc(4,callback);
+calc(11,callback);
+calc(7,callback);
