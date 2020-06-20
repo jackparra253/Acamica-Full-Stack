f046270c2f229c7405a7c99d9bb09bb7d3f6034a
Jack <jack.parra253@gmail.com>
2020-06-20 16:02:24 -0500

    exercises functions with array

---
+++ src/core/m_27_05_exercise.js
@@ -0,0 +1,13 @@
+
+function x(callback, array) { }
+
+function agregarElementoAlInicioArray(array, element) {
+    array.unshift(element);
+}
+
+
+function agregarElementoAlFinalArray(array, element) {
+    array.push(element);
+}
+
+const eliminar = (array) => { array.pop() }
